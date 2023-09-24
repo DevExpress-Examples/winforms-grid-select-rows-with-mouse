759300dafe826cadde6d3c27e7d7364413c06c0c
DevExpressExampleBot <fakeEmail>
2023-09-24 14:13:45 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,58 +1,57 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim TempXViewsPrinting As DevExpress.XtraGrid.Design.XViewsPrinting = New DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1)
-			gridView1.OptionsSelection.MultiSelect = True
-			gridView1.OptionsBehavior.Editable = False
-			gridView1.OptionsSelection.EnableAppearanceFocusedCell = False
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub SelectRows(ByVal view As GridView, ByVal startRow As Integer, ByVal endRow As Integer)
-			If startRow > -1 AndAlso endRow > -1 Then
-				view.BeginSelection()
-				view.ClearSelection()
-				view.SelectRange(startRow, endRow)
-				view.EndSelection()
-			End If
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Function GetRowAt(ByVal view As GridView, ByVal x As Integer, ByVal y As Integer) As Integer
-			Return view.CalcHitInfo(New Point(x, y)).RowHandle
-		End Function
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim tmp_XViewsPrinting = New DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1)
+            gridView1.OptionsSelection.MultiSelect = True
+            gridView1.OptionsBehavior.Editable = False
+            gridView1.OptionsSelection.EnableAppearanceFocusedCell = False
+        End Sub
 
-		Private StartRowHandle As Integer = -1
-		Private CurrentRowHandle As Integer = -1
+        Private Sub SelectRows(ByVal view As GridView, ByVal startRow As Integer, ByVal endRow As Integer)
+            If startRow > -1 AndAlso endRow > -1 Then
+                view.BeginSelection()
+                view.ClearSelection()
+                view.SelectRange(startRow, endRow)
+                view.EndSelection()
+            End If
+        End Sub
 
-		Private Sub gridView1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles gridView1.MouseDown
-			StartRowHandle = GetRowAt(TryCast(sender, GridView), e.X, e.Y)
-		End Sub
+        Private Function GetRowAt(ByVal view As GridView, ByVal x As Integer, ByVal y As Integer) As Integer
+            Return view.CalcHitInfo(New Point(x, y)).RowHandle
+        End Function
 
-		Private Sub gridView1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles gridView1.MouseMove
-			Dim newRowHandle As Integer = GetRowAt(TryCast(sender, GridView), e.X, e.Y)
-			If CurrentRowHandle <> newRowHandle Then
-				CurrentRowHandle = newRowHandle
-				SelectRows(TryCast(sender, GridView), StartRowHandle, CurrentRowHandle)
-			End If
-		End Sub
+        Private StartRowHandle As Integer = -1
 
-		Private Sub gridView1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles gridView1.MouseUp
-			StartRowHandle = -1
-			CurrentRowHandle = -1
-		End Sub
-	End Class
+        Private CurrentRowHandle As Integer = -1
+
+        Private Sub gridView1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+            StartRowHandle = GetRowAt(TryCast(sender, GridView), e.X, e.Y)
+        End Sub
+
+        Private Sub gridView1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim newRowHandle As Integer = GetRowAt(TryCast(sender, GridView), e.X, e.Y)
+            If CurrentRowHandle <> newRowHandle Then
+                CurrentRowHandle = newRowHandle
+                SelectRows(TryCast(sender, GridView), StartRowHandle, CurrentRowHandle)
+            End If
+        End Sub
+
+        Private Sub gridView1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs)
+            StartRowHandle = -1
+            CurrentRowHandle = -1
+        End Sub
+    End Class
 End Namespace
